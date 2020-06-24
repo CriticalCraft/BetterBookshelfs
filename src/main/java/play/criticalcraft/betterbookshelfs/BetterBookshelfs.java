d1b296964095284175baf19d2978fa134293db3f
Woefie <larsh.hanssen@gmail.com>
2020-06-24 21:45:31 +0200

    First commit

---
+++ src/main/java/play/criticalcraft/betterbookshelfs/BetterBookshelfs.java
@@ -0,0 +1,17 @@
+package play.criticalcraft.betterbookshelfs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterBookshelfs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
